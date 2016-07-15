17ad436ca0c4eada988f48e51d76e14097a9254b
Wolfgang Herget <wolfgang.herget@dfki.de>
2016-07-15 17:27:12 +0200

    Extend integration test to figure out usage scenario

---
+++ src/main/java/org/cyberborean/rdfbeans/mapper/RDFMapUsing.java
@@ -0,0 +1,12 @@
+package org.cyberborean.rdfbeans.mapper;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RDFMapUsing {
+	Class<?> value();
+}
--- src/test/java/org/cyberborean/rdfbeans/test/mapper/RDFMapperIntegrationTest.java
+++ src/test/java/org/cyberborean/rdfbeans/test/mapper/RDFMapperIntegrationTest.java
@@ -3,13 +3,18 @@ package org.cyberborean.rdfbeans.test.mapper;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestFactory;
+import org.apache.http.message.BasicHttpRequest;
 import org.cyberborean.rdfbeans.annotations.RDF;
 import org.cyberborean.rdfbeans.annotations.RDFBean;
 import org.cyberborean.rdfbeans.annotations.RDFNamespaces;
 import org.cyberborean.rdfbeans.exceptions.RDFBeanException;
 import org.cyberborean.rdfbeans.mapper.MapperFactory;
+import org.cyberborean.rdfbeans.mapper.RDFMapUsing;
 import org.cyberborean.rdfbeans.mapper.RDFMapper;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
@@ -18,9 +23,13 @@ import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.IsAnything.any;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItems;
 
 public class RDFMapperIntegrationTest {
 
@@ -30,6 +39,20 @@ public class RDFMapperIntegrationTest {
 		@RDF("ex:stringProperty")
 		@Getter @Setter
 		public String stringProperty;
+
+		@RDF("ex:sampleList")
+		@Getter @Setter
+		public List<String> listProperty;
+
+		@RDF("ex:custom")
+		@RDFMapUsing(CustomMapper.class)
+		public HttpRequest custom;
+	}
+
+	public static class CustomMapper {
+		public HttpRequest deserialize(Resource resource, RepositoryConnection conn) {
+			return new BasicHttpRequest("GET", resource.stringValue());
+		}
 	}
 
 	private SailRepository repo;
@@ -54,5 +77,13 @@ public class RDFMapperIntegrationTest {
 		assertThat(deserialized, notNullValue());
 		assertThat(deserialized.stringProperty, notNullValue());
 		assertThat(deserialized.stringProperty, is("stringValue"));
+
+		assertThat(deserialized.listProperty, notNullValue());
+		assertThat(deserialized.listProperty.size(), is(3));
+		assertThat(deserialized.listProperty, hasItems(any(String.class), any(String.class), any(String.class)));
+
+		assertThat(deserialized.custom, any(HttpRequest.class));
+		assertThat(deserialized.custom.getRequestLine().getMethod(), is("GET"));
+		assertThat(deserialized.custom.getRequestLine().getUri(), is("http://example.com/customTarget"));
 	}
 }
